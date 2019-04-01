112459108b59091c684aa076b3708ac68813b186
JackGonzo11 <47435101+JackGonzo11@users.noreply.github.com>
2019-04-01 10:01:51 -0400

    Add files via upload

---
+++ Drag.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import math
+import rospy
+from geometry_msgs.msg import WrenchStamped
+from nav_msgs.msg import Odometry
+
+
+Linear_Drag_X = 0
+Linear_Drag_Y = 0
+Max_Velocity = 0
+Current_VelocityX = 0
+Compare_Velocties = math.abs(Max_Velocity - Current_VelocityX)
+
+
+
+def callback(data):
+	global Linear_Drag_X, Max_Velocity, Current_VelocityX
+	Current_VelocityX = data.twist.twist.linear.x
+	
+	
+	if Max_Velocity < Current_VelocityX:
+		Max_Velocity = Current_VelocityX
+	#else:
+	#	force_msg.wrench.force.x = 0
+	#	pub.publish(force_msg)
+	Linear_Drag_X = (20 / Max_Velocity)
+	
+	
+
+def Print_Linear_Drag():
+	rospy.loginfo('Linear Drag X:  {}, Linear Drag Y:  {}'.format(Linear_Drag_X,Linear_Drag_Y))
+
+
+
+def Apply_Force():
+	pub = rospy.Publisher('/wrench', WrenchStamped, queue_size=20)
+	rospy.init_node('move_sub', anonymous=False)
+	force_msg = WrenchStamped()
+	force_msg.wrench.force.x = 20
+	
+	
+	
+
+
+
+	while not rospy.is_shutdown():
+		if Compare_Velocties != .000001:
+			pub.publish(force_msg)
+			rospy.Subscriber("/odom", Odometry, callback)
+		Print_Linear_Drag()
+		
+		
+		
+		
+		
+
+if __name__== '__main__':
+	try:
+		Apply_Force()
+	except rospy.ROSInterruptException:
+		pass
